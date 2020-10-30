9538b79970a6ec57f5f488d307007a9740ab8e1b
pear-pitchapa <73691034+pear-pitchapa@users.noreply.github.com>
2020-10-30 17:44:21 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>The Cat</h1>
+        <p>Kitten</p>
+        <img src="cat.jpg">
+        <br>
+        <a href "http://www.it.kmitl.ac.th">this is Link</a>
+
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+
+    </body>
+</html>
